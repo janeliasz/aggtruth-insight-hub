e2a7741704888b0bbbb4ea0397980b7e56ff7ac8
janeliasz <69108816+janeliasz@users.noreply.github.com>
2025-06-23 14:22:32 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/aggtruth-insight-hub/",
+  base: "/aggtruth-insight-hub/",
   server: {
     host: "::",
     port: 8080,
